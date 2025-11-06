31a7b7c895127c5d09f164c18b5d061958c11af3
Yasmim <yasmimmantovani10@gmail.com>
2025-11-06 10:39:40 -0300

    .

--- PHP/consulta.php
+++ PHP/consulta.php
@@ -12,7 +12,7 @@ $mensagem = "";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../CSS/tabela2.css">
   <link rel="shortcut icon" href="../img/pngegg.png">
-  <title>Resultado da Consulta</title>
+  <title>Consulta individual</title>
 </head>
 <body>
     <div class="container">
